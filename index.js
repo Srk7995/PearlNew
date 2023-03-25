b94a30d58afec0d81fcc5d84f75778786c803865
root <root@node1>
2023-03-25 19:13:22 +0000

    File Added

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+const app= express()
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+/* istanbul ignore next */
+
+  app.listen(3000);
+  console.log('Express started on port 3000');
+
